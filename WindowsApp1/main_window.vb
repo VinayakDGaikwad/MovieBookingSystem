2a250fb617d742712f6fb9b3a3d0586195b764dc
Administrator <vinayakdwarkobagaikwad@gmail.com>
2022-04-22 04:41:52 +0000

    Add project files.

---
+++ WindowsApp1/main_window.vb
@@ -0,0 +1,21 @@
+﻿Imports MySqlConnector
+
+Public Class main_window
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conn As MySqlConnection
+        conn = New MySqlConnection("server=localhost;user id=root;password=89660;database=movies;persistsecurityinfo=True")
+        conn.Open()
+        Dim query As String
+        query = " show databases; "
+        Dim results
+        Dim Command As New MySqlCommand(query, conn)
+        Dim rd As MySqlDataReader
+        rd = Command.ExecuteReader()
+        Do While rd.Read()
+            results = results & rd.GetString(0) & vbTab
+        Loop
+        MsgBox("Data saved Successfully" + results, MsgBoxStyle.MsgBoxRight, "Done")
+        conn.Close()
+    End Sub
+
+End Class
